383b3ee0baf4fe60fed1e856de3e9f4ae6841db1
algoidan <idan@algorand.com>
2022-04-28 14:46:07 +0300

    lower max depth for tests

--- crypto/merklearray/merkle.go
+++ crypto/merklearray/merkle.go
@@ -29,7 +29,7 @@ import (
 const (
 	// MaxEncodedTreeDepth is the maximum tree depth (root only depth 0) for a tree which
 	// is being encoded (either by msbpack or by the fixed length encoding)
-	MaxEncodedTreeDepth = 16
+	MaxEncodedTreeDepth = 4
 
 	// MaxNumLeavesOnEncodedTree is the maximum number of leaves allowed for a tree which
 	// is being encoded (either by msbpack or by the fixed length encoding)
