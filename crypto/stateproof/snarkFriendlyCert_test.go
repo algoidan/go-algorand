258961371fd56b8538047335373a034c536b3441
algoidan <idan@algorand.com>
2022-04-28 14:43:46 +0300

    refactor test to create a small cert

--- crypto/stateproof/snarkFriendlyCert_test.go
+++ crypto/stateproof/snarkFriendlyCert_test.go
@@ -22,25 +22,78 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/algorand/go-algorand/crypto"
+	"github.com/algorand/go-algorand/crypto/merklearray"
+	"github.com/algorand/go-algorand/crypto/merklesignature"
+	"github.com/algorand/go-algorand/data/basics"
 	"github.com/algorand/go-algorand/test/partitiontest"
 )
 
 func TestCertToJSON(t *testing.T) {
 	partitiontest.PartitionTest(t)
-
 	a := require.New(t)
 
-	p := generateCertForTesting(a, 4)
-	cert := p.cc
-	verif, err := MkVerifier(p.partCommitment, p.provenWeight, 4)
+	totalWeight := 10000000
+	npartHi := 3
+	npartLo := 5
+	npart := npartHi + npartLo
+	const targetForSampleCert = 4
+
+	data := testMessage("hello world").IntoStateProofMessageHash()
+	provenWt := uint64(totalWeight / 2)
+
+	var parts []basics.Participant
+	var sigs []merklesignature.Signature
+	for i := 0; i < npartHi; i++ {
+		part, sig := createParticipantAndSignature(a, totalWeight, npartHi, data)
+		parts = append(parts, part)
+		sigs = append(sigs, sig)
+	}
+
+	for i := 0; i < npartLo; i++ {
+		part, sig := createParticipantAndSignature(a, totalWeight, npartLo, data)
+		parts = append(parts, part)
+		sigs = append(sigs, sig)
+	}
+
+	partcom, err := merklearray.BuildVectorCommitmentTree(basics.ParticipantsArray(parts), crypto.HashFactory{HashType: HashType})
 	a.NoError(err)
 
-	err = verif.Verify(compactCertRoundsForTests, p.data, &cert)
+	b, err := MkBuilder(data, compactCertRoundsForTests, uint64(totalWeight/2), parts, partcom, targetForSampleCert)
+	a.NoError(err)
+
+	for i := 0; i < npart; i++ {
+		a.False(b.Present(uint64(i)))
+		a.NoError(b.IsValid(uint64(i), sigs[i], true))
+		b.Add(uint64(i), sigs[i])
+	}
+
+	cert, err := b.Build()
+	a.NoError(err)
+
+	verif, err := MkVerifier(partcom.Root(), provenWt, targetForSampleCert)
+	a.NoError(err)
+
+	err = verif.Verify(compactCertRoundsForTests, data, cert)
 	a.NoError(err, "failed to verify the compact cert")
 
-	certenc, err := cert.createSnarkFriendlyCert(p.data[:])
+	certenc, err := cert.createSnarkFriendlyCert(data[:])
 	a.NoError(err)
 
 	fmt.Printf(toZokCode(certenc, verif, testMessage("hello world").IntoStateProofMessageHash(), compactCertRoundsForTests))
 
+}
+
+func createParticipantAndSignature(a *require.Assertions, totalWeight int, npartHi int, data StateProofMessageHash) (basics.Participant, merklesignature.Signature) {
+	key := generateTestSigner(0, uint64(compactCertRoundsForTests)*7+1, compactCertRoundsForTests, a)
+	part := basics.Participant{
+		PK:     *key.GetVerifier(),
+		Weight: uint64(totalWeight / 2 / npartHi),
+	}
+
+	signerInRound := key.GetSigner(compactCertRoundsForTests)
+	sig, err := signerInRound.SignBytes(data[:])
+	a.NoError(err, "failed to create keys")
+
+	return part, sig
 }
